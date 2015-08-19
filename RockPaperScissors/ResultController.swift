49a6b08e4f406600364d5b1174e601f4c4b2ac46
Ten Mutunhire <tmutunhire@gmail.com>
2015-08-19 13:35:51 +0200

    create game

--- RockPaperScissors/ResultController.swift
+++ RockPaperScissors/ResultController.swift
@@ -1,29 +1,24 @@
 //
-//  ViewController.swift
+//  ResultController.swift
 //  RockPaperScissors
 //
 //  Created by Tomahawk Africa Tindo on 8/19/15.
 //  Copyright (c) 2015 Tomahawk Africa Tindo. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
+class ResultController: UIViewController {
     
-    @IBOutlet weak var rock: UIButton?
-    @IBOutlet weak var paper: UIButton?
-    @IBOutlet weak var scissors: UIButton?
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    
 }
-
